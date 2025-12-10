adc7d06e98f2e25c5d12c9393ff4cb856bec06c9
REDY-a <alice.zqr233@gmail.com>
2025-12-10 11:35:10 -0600

    Fixed inconsistent API_URI in config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const API_URL = import.meta.env.PROD 
   ? 'https://p64.onrender.com'  // Your deployed backend URL
-  : 'http://localhost:5000/api';         // Local development
+  : 'http://localhost:5000/';         // Local development
